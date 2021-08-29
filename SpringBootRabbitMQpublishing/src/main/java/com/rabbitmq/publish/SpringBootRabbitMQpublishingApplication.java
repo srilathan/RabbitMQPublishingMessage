3102b83103f643ce75de66ffe3850099abef362c
IN_MOURITECH\srilathan <srilathan.n@mouritech.com>
2021-08-29 09:35:13 +0530

    springbootrabbitmqpublish project

---
+++ SpringBootRabbitMQpublishing/src/main/java/com/rabbitmq/publish/SpringBootRabbitMQpublishingApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.publish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitMQpublishingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitMQpublishingApplication.class, args);
+	}
+
+}
